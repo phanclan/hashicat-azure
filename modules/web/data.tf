a820d1b29b3ac93b4917921e584c7460fa574733
Peter Phan <pphan@hashicorp.com>
2020-12-09 16:48:30 -0800

    refactoring as modules

---
+++ modules/web/data.tf
@@ -0,0 +1,13 @@
+//--------------------------------------------------------------------
+// Workspace Data
+data "terraform_remote_state" "pphan_servicenow_hashicat_azure_network" {
+  backend = "remote"
+  config = {
+    organization = "pphan-servicenow"
+    workspaces = { name = "hashicat-azure-network" }
+  }
+}
+
+# data "azurerm_resource_group" "network" {
+#   name = azurerm_resource_group.myresourcegroup.name
+# }
---
+++ modules/web/hashicat.tf
@@ -0,0 +1,118 @@
+resource "azurerm_network_interface" "catapp-nic" {
+  name                      = "${var.prefix}-catapp-nic"
+  location                  = var.location
+  resource_group_name       = data.terraform_remote_state.pphan_servicenow_hashicat_azure_network.outputs.rg_name
+  #azurerm_resource_group.myresourcegroup.name
+  network_security_group_id = data.terraform_remote_state.pphan_servicenow_hashicat_azure_network.outputs.sg_id
+
+  ip_configuration {
+    name                          = "${var.prefix}ipconfig"
+    subnet_id                     = data.terraform_remote_state.pphan_servicenow_hashicat_azure_network.outputs.subnet_id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.catapp-pip.id
+  }
+}
+
+resource "azurerm_public_ip" "catapp-pip" {
+  name                = "${var.prefix}-ip"
+  location            = var.location
+  resource_group_name = data.terraform_remote_state.pphan_servicenow_hashicat_azure_network.outputs.rg_name
+  allocation_method   = "Dynamic"
+  domain_name_label   = "${var.prefix}-meow"
+}
+
+resource "azurerm_virtual_machine" "catapp" {
+  name                = "${var.prefix}-meow"
+  location            = var.location
+  resource_group_name = data.terraform_remote_state.pphan_servicenow_hashicat_azure_network.outputs.rg_name
+  vm_size             = var.vm_size
+
+  network_interface_ids         = [azurerm_network_interface.catapp-nic.id]
+  delete_os_disk_on_termination = "true"
+
+  storage_image_reference {
+    publisher = var.image_publisher
+    offer     = var.image_offer
+    sku       = var.image_sku
+    version   = var.image_version
+  }
+
+  storage_os_disk {
+    name              = "${var.prefix}-osdisk"
+    managed_disk_type = "Standard_LRS"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+  }
+
+  os_profile {
+    computer_name  = var.prefix
+    admin_username = var.admin_username
+    admin_password = var.admin_password
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
+  tags = {}
+}
+
+# We're using a little trick here so we can run the provisioner without
+# destroying the VM. Do not do this in production.
+
+# If you need ongoing management (Day N) of your virtual machines a tool such
+# as Chef or Puppet is a better choice. These tools track the state of
+# individual files and can keep them in the correct configuration.
+
+# Here we do the following steps:
+# Sync everything in files/ to the remote VM.
+# Set up some environment variables for our script.
+# Add execute permissions to our scripts.
+# Run the deploy_app.sh script.
+resource "null_resource" "configure-cat-app" {
+  depends_on = [
+    azurerm_virtual_machine.catapp,
+  ]
+
+  # Terraform 0.11
+  # triggers {
+  #   build_number = "${timestamp()}"
+  # }
+
+  # Terraform 0.12
+  triggers = {
+    build_number = timestamp()
+  }
+
+  provisioner "file" {
+    source      = "files/"
+    destination = "/home/${var.admin_username}/"
+
+    connection {
+      type     = "ssh"
+      user     = var.admin_username
+      password = var.admin_password
+      host     = azurerm_public_ip.catapp-pip.fqdn
+    }
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt -y update",
+      "sudo apt -y install apache2",
+      "sudo systemctl start apache2",
+      "sudo chown -R ${var.admin_username}:${var.admin_username} /var/www/html",
+      "chmod +x *.sh",
+      "PLACEHOLDER=${var.placeholder} WIDTH=${var.width} HEIGHT=${var.height} PREFIX=${var.prefix} ./deploy_app.sh",
+      "sudo apt -y install cowsay",
+      "cowsay Mooooooooooo!",
+    ]
+
+    connection {
+      type     = "ssh"
+      user     = var.admin_username
+      password = var.admin_password
+      host     = azurerm_public_ip.catapp-pip.fqdn
+    }
+  }
+}
