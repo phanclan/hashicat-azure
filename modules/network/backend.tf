5389e04051735e07deb5aec45bdcc7fa359ea5d0
Peter Phan <pphan@hashicorp.com>
2020-12-09 16:49:07 -0800

    refactoring as modules

---
+++ modules/network/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "pphan-servicenow"
+
+    workspaces {
+      name = "hashicat-azure-network"
+    }
+  }
+}
